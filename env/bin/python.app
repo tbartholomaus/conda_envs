7ca87a17654b61b040515bf20bf701572c06b901
Tim Bartholomaus <24902730+tbartholomaus@users.noreply.github.com>
2021-09-17 09:00:18 -0700

    fixing gitignore

--- env/bin/python.app
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export PYTHONEXECUTABLE=/Users/timb/Documents/syncs/OneDrive - University of Idaho/RESEARCHs/git_repos/conda_envs/env/bin/python
-/Users/timb/Documents/syncs/OneDrive - University of Idaho/RESEARCHs/git_repos/conda_envs/env/python.app/Contents/MacOS/python "$@"
--- env/lib/xsltConf.sh
+++
@@ -1,7 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/Users/timb/Documents/syncs/OneDrive - University of Idaho/RESEARCHs/git_repos/conda_envs/env/lib"
-XSLT_LIBS="-lxslt -L/Users/timb/Documents/syncs/OneDrive - University of Idaho/RESEARCHs/git_repos/conda_envs/env/lib -lxml2 -L/Users/timb/Documents/syncs/OneDrive - University of Idaho/RESEARCHs/git_repos/conda_envs/env/lib -L/Users/timb/Documents/syncs/OneDrive - University of Idaho/RESEARCHs/git_repos/conda_envs/env/lib -lpthread -L/Users/timb/Documents/syncs/OneDrive - University of Idaho/RESEARCHs/git_repos/conda_envs/env/lib -liconv -L/Users/timb/Documents/syncs/OneDrive - University of Idaho/RESEARCHs/git_repos/conda_envs/env/lib -licui18n -licuuc -licudata -lm  "
-XSLT_INCLUDEDIR="-I/Users/timb/Documents/syncs/OneDrive - University of Idaho/RESEARCHs/git_repos/conda_envs/env/include"
-MODULE_VERSION="xslt-1.1.33"
